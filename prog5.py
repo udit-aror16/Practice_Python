3c63fb3f63eb31242dc17eb395e5bded94dd6486
udit-aror16 <uauditarora16@gmail.com>
2025-04-23 16:56:13 +0530

    string_reverse

---
+++ prog5.py
@@ -0,0 +1,9 @@
+# Program to reverse a given string : 
+
+str=input("Enter the string you want to reverse: ")
+
+# Reverse the string using slicing
+reversed_str = str[::-1]
+
+# Display the result
+print(f"Reversed string = {reversed_str}")
