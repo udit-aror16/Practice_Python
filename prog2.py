1677011ab00edf78a91188405ccda5a996e1385a
udit-aror16 <uauditarora16@gmail.com>
2025-04-23 16:00:00 +0530

    list_sorting

---
+++ prog2.py
@@ -0,0 +1,6 @@
+# Creates a list of 5 names and sort them alphabetically using python : 
+
+strlist=["udit","rohit","mohit","aniket","rohan"]
+strlist.sort()
+print("Sorted names = ",end="")
+print(strlist)
